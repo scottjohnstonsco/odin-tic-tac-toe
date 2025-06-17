0a42c8e68f4a2ebac8f2cb20fa69f3aeccd9ffd9
Scott Johnston <209605268+scottjohnstonsco@users.noreply.github.com>
2025-06-17 15:43:03 +0900

    Added gameboard and game start logic, working on players being added

---
+++ script.js
@@ -0,0 +1,41 @@
+const Gameboard = (() => {
+    let gameboard = ["","","","","","","","","" ]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id=square-${index}>${square}</div>`
+        })
+    }
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+
+    return{
+        render,
+    }
+}) ();
+
+const createPlayer = (name, mark) => {
+    return{
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+        let players = [];
+        let currentPlayerIndex = 0;
+        let gameOver = false;
+
+        const start = () => {
+            players = [
+                createPlayer(document.querySelector("#player1").value, "X",
+                createPlayer(document.querySelector("#player2").value, "O",
+            ]
+        }
+})();
+
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", ()=>{
+    // Gamepad.start();
+})
